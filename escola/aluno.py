84c36227e4dfc3325a89b4997b170d2ffb7a841d
AliciaPavao <aliciapavao369@gmail.com>
2025-08-05 11:31:20 -0300

    add: verififcao de erro

--- escola/aluno.py
+++ escola/aluno.py
@@ -1,9 +1,5 @@
 def calcular_media(notas: list[float]) -> float:
 
-    # Verificando se é uma lista
-    if not isinstance(notas, list[float]):
-        raise ValueError("as notas não são uma lista")
-
     # Validando se a lista é vazia
     if len(notas) == 0:
         raise ValueError("não é permitido uma lista vazia")
