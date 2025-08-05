b36c81ebfe2328094a9dd99ad219c3511a790b28
AliciaPavao <aliciapavao369@gmail.com>
2025-08-05 10:25:55 -0300

    Função para calcular média

---
+++ escola/aluno.py
@@ -0,0 +1,3 @@
+def calcular_media(notas: list[float]) -> float
+    media = sum(notas) /len(notas)
+    return media
--- tests/test_calc_media_basico.py
+++ tests/test_calc_media_basico.py
@@ -1,9 +1,11 @@
 from escola.aluno import calcular_media
 
 def test_calcular_media_entrada_basica_sem_erro():
-    #Defina as entradas     
+    # Defina as entradas     
     entrada = [10.0, 5.0, 6.0, 1.0]
 
+    # Executa o código
     resultado = calcular_media(entrada)
 
+    # Checa a saída
     assert resultado == 5.5
