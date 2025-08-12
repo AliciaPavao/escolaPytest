3eab4d8b0c69baf735dc35d6726bd336f196df2c
AliciaPavao <aliciapavao369@gmail.com>
2025-08-12 10:49:16 -0300

    finção de validar se os itens na lista sao numeros e se são maiores que 10 e menores q 0

--- escola/aluno.py
+++ escola/aluno.py
@@ -1,8 +1,39 @@
 def calcular_media(notas: list[float]) -> float:
 
+    """"
+    Calcula a média de uma lista de notas.
+
+    Parâmetros:
+    notas (list[float]): Lista de notas a serem calculadas.
+
+    Retorna:
+    float: A média das notas, arredondada para uma casa decimal.
+    """
+    # Validando se tem uma string
+    if not isinstance(notas, list):
+        raise TypeError("nota invalida")
+    
+    # Validando se tem uma string 2.0
+    # if type(notas) != list:
+    #     raise TypeError("nota invalida")
+    
     # Validando se a lista é vazia
     if len(notas) == 0:
         raise ValueError("não é permitido uma lista vazia")
+
+    # Validando se todos os elementos da lista são numeros (int e float)
+    for nota in notas:
+        if not isinstance(nota, (int, float)):
+            raise TypeError("não é permitido uma string na lista")
+        
+        if nota > 10:
+            raise ValueError("limite da nota [0, 10]")
+        
+        if nota < 0:
+            raise ValueError("limite da nota [0, 10]")
+    
+    
+
 
     media = sum(notas) /len(notas)
     return round(media, 1)
--- tests/test_calc_media_excessoes.py
+++ tests/test_calc_media_excessoes.py
@@ -9,3 +9,39 @@ def test_calcular_media_lista_vazia():
     # Executando a função e esperando erro
     with pytest.raises(ValueError, match="não é permitido uma lista vazia"):
         calcular_media(entrada)
+
+def test_calcular_media_enviando_string():
+    
+    # Definindo a entrada
+    entrada = ""
+
+    # Executando a função e esperando erro
+    with pytest.raises(TypeError, match="nota invalida"):
+        calcular_media(entrada)
+
+def test_calcular_media_string_lista():
+    
+    # Definindo a entrada
+    entrada = [(3,8),20]
+
+    # Executando a função e esperando erro
+    with pytest.raises(TypeError, match="não é permitido uma string na lista"):
+        calcular_media(entrada) 
+
+def test_calcular_media_num_negativo():
+    
+    # Definindo a entrada
+    entrada = [-10.0]
+
+    # Executando a função e esperando erro
+    with pytest.raises(ValueError, match="limite da nota \[0, 10\]"):
+        calcular_media(entrada) 
+
+def test_calcular_media_num_maiordeDez():
+    
+    # Definindo a entrada
+    entrada = [11.0]
+
+    # Executando a função e esperando erro
+    with pytest.raises(ValueError, match="limite da nota \[0, 10\]"):
+        calcular_media(entrada) 
